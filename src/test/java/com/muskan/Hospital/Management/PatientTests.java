dc03ca33121bc6ee0ca9e1afa976d690e66a22c4
Muskan Chaudhary <muskanchaudhary152005.com>
2026-01-28 21:52:54 +0530

    Added patient entity updates and service tests

--- src/test/java/com/muskan/Hospital/Management/PatientTests.java
+++ src/test/java/com/muskan/Hospital/Management/PatientTests.java
@@ -1,11 +1,14 @@
 package com.muskan.Hospital.Management;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.muskan.Hospital.Management.entity.Patient;
 import com.muskan.Hospital.Management.repository.PatientRepository;
+import com.muskan.Hospital.Management.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @SpringBootTest
@@ -14,8 +17,35 @@ public class PatientTests {
     @Autowired
 
     private PatientRepository patientRepository;
+
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public  void testPatientRepository(){
-       List<Patient> patientList = patientRepository.findAll();
+
+        List<Patient> patients = patientRepository.findAll();
+        System.out.println("Number of patients: " + patients.size());
+        patients.forEach(System.out::println);
+    }
+
+    @Test
+    public void testTransactionMethods(){
+//         Patient patient= patientService.getPatientById(1L);
+//
+//        System.out.println(patient);
+
+//        Patient patient = patientRepository.findByName("William Brown");
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1988,7,30), "benjamin.thomas@example.com");
+
+//        List<Patient> patientList = patientRepository.findByNameContaining("en");
+
+//        List<Patient> patientList = patientRepository.findByGender("Female");
+
+        List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1987,3,10));
+
+        for(Patient patient : patientList){
+            System.out.println(patient);
+        }
     }
 }
--- src/test/java/com/muskan/Hospital/Management/entity/Patient.java
+++ src/test/java/com/muskan/Hospital/Management/entity/Patient.java
@@ -1,24 +1,92 @@
 package com.muskan.Hospital.Management.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@ToString
+@Table(
+        name = "patient_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_patient_email", columnNames = {"email"}),
+                @UniqueConstraint(name = "unique_patient_name_birthDate", columnNames = {"name","birthDate"}),
+        },
+        indexes = {
+                @Index(name = "idx_patient_birth_data",columnList = "birthDate")
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+    private Long id;
 
+//    @Column(name  = "patient_name")
     private String name;
 
+    @ToString.Exclude
     private LocalDate birthDate;
 
     private String email;
 
     private String gender;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    //    @Override
+//    public String toString() {
+//        return "Patient{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", birthDate=" + birthDate +
+//                ", email='" + email + '\'' +
+//                ", gender='" + gender + '\'' +
+//                '}';
+//    }
 }
